80f7ee0cc27d548ed982029fe97a23183a8657d8
BootCampBL <satyabhandari43@gmail.com>
2021-08-09 18:03:33 +0530

    [satya] added- Deleting Person contact by its name

--- Address-Book-System/Program.cs
+++ Address-Book-System/Program.cs
@@ -8,10 +8,21 @@ namespace Address_Book_System
         {
             Console.WriteLine("Welcome to Address Book System");
 
-            AddressBook.AddContact();
-            AddressBook.ContactsDisplay();
+            //AddressBook.AddContact();
+            //AddressBook.ContactsDisplay();  uc2
 
+            //AddressBook.EditContact();       uc3
+            //AddressBook.ContactsDisplay();
+
+            AddressBook.AddContact();
+            Console.WriteLine("\n");
+            AddressBook.ContactsDisplay();
+            Console.WriteLine("\n");
             AddressBook.EditContact();
+            Console.WriteLine("\n");
+            AddressBook.ContactsDisplay();
+            Console.WriteLine("\n");
+            AddressBook.DeleteContact();
             AddressBook.ContactsDisplay();
         }
     }
